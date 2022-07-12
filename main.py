35018663fcf1075a3835a4c20d1d4fbb34368ff0
Shinwaffle <shinwaffle@gmail.com>
2022-07-11 21:07:05 -0500

    pygsheets acc env

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import interactions
 import logging
 import os
 TOKEN = os.environ.get("TOKEN")
-PYGSHEETS_BOTACC_PATH = ".service.json"
+PYGSHEETS_BOTACC_PATH = os.environ.get("PYGSHEETS_BOTACC_PATH")
 
 logging.basicConfig(level=logging.INFO)
 
